5f46f247b9ad3781b8a33828d618ea5c81b69a92
newwbiiee <142647441+newwbiiee@users.noreply.github.com>
2023-08-19 05:23:51 +0000

    container config done

---
+++ basics/vlc.js
@@ -0,0 +1,17 @@
+const accountId=14457
+let accountEmail ="aryanbhofficial@gmail.com"
+var accountpassword="12345"
+accountCity="Jaipur"
+
+// accountId=2 not allowed
+accountEmail="he@gmail.com"
+accountpassword+"23456172"
+accountCity="delhi"
+let accountstate;
+
+console.table([accountEmail,accountId,accountpassword,accountstate])
+console.log(accountId);
+
+
+// prfer no to use var 
+// because of issue in blok scope and functional scope
