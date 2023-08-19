f08249fe1fee89cedc8e98f5c07859fa7945e87a
newwbiiee <142647441+newwbiiee@users.noreply.github.com>
2023-08-19 06:27:16 +0000

    String to number

---
+++ basics/stN.js
@@ -0,0 +1,22 @@
+///OPERATIONSSSSSSSSSS...S..S.S.S.;S.
+
+// stn= String to number conversionn
+let value =3;
+let negvalue=-value;
+console.log(negvalue)
+
+// let v1=5;
+// console.log((v1*v1)*(v1*v1));
+
+
+// let str ="hello"
+// let str1="errors"
+
+// let str2= str+str1;
+// console.log(str2)
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
