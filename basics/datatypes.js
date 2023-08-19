a6933fae5881c25c1224288bb51436942dc0dfde
newwbiiee <142647441+newwbiiee@users.noreply.github.com>
2023-08-19 05:41:22 +0000

    dataty

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newe version
+
+// alert("hello errors") we are using node js not browser and alert is only applicable in brower
+console.log(3+3);
+console.log("hitesh")
+
+
+let name ="errors"
+let age=18;
+let islogggesin=false;
+
+//number = 2 to power 53
+//biint
+// string=""
+//boolean=true/false
+//null=standalone
+//undefined=
+//symbol=unique
+
+
+//object
+
+console.log(typeof "errors")
+console.log(typeof 23);
+console.log(typeof 1.23)
+
+console.log(typeof null);//object
+console.log(typeof undefined)//undefined
