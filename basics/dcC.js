a0518742903254a2eb8317e99b1c5df549183d66
newwbiiee <142647441+newwbiiee@users.noreply.github.com>
2023-08-19 06:08:47 +0000

    errors

---
+++ basics/dcC.js
@@ -0,0 +1,55 @@
+let score ="34abs"
+let s1=undefined
+let s2=true
+let s3=null
+
+
+console.log(typeof s3)
+console.log(typeof s2)
+console.log(typeof s1)
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueNumber =Number(score)
+
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+
+conversionnnn
+// "33" can be  converted into 33
+// "33abc" can be converted into NaN
+
+true = 1;false =0;
+
+
+
+
+let isLoggedIN=0;//and ths will false
+let isLoggedIN2=1//will be converted into true
+
+
+let isLoggedIN3=""//will lead us to false
+let isLoggedIN4="errors";// will lead us to true
+
+let booleanisLoggedIn4=isLoggedIN4;
+console.log(booleanisLoggedIn4);
+
+let booleanisLoggedIn3=Boolean(isLoggedIN3);
+console.log(booleanisLoggedIn3)
+
+let booleanisLoggedIn2=Boolean(isLoggedIN2);
+console.log(booleanisLoggedIn2);
+
+let booleanisLoggedIn = Boolean(isLoggedIN);
+console.log(booleanisLoggedIn)
+
+let someNumber =23;
+let stringNumbr =String(someNumber)
+console.log(stringNumbr)
+
+console.log(typeof stringNumbr); // the view is  of the integer
+// bt it is the string bcs we have changed to it
+
+
+
