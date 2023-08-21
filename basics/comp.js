666f2537b6829e1e62a455454263e083b6dee984
newwbiiee <142647441+newwbiiee@users.noreply.github.com>
2023-08-21 03:03:27 +0000

    conatainer config

---
+++ basics/comp.js
@@ -0,0 +1,25 @@
+//comparisison operatorsssss
+
+// console.log("2" >1)
+// console.log("02">1)
+
+// console.log(null >0) //(1)
+// console.log(null == 0)// (2)
+// console.log(null>=0)//(3)
+
+console.log(undefined ==0) //(4)
+console.log(undefined >0)
+console.log(undefined <0);
+//same rules are applied for the undefined
+
+
+
+//(====) this will also check for the type of datatype and if they are equal 
+//and the type of their data type is also equal then it will return true otherwise false
+console.log(2===2)// otherwise  false
+console.log("2"===2);
+
+
+// the reaspn why tht an equality check == and comparision >< >= , <= 
+//work diffrently is thatcomparision convert null to 
+//a number treating it as 0 thts why (3) >=0 is true and (1) is false
---
+++ basics/ds.js
@@ -0,0 +1,50 @@
+//primitive datatype - 7 typess
+// String
+// NUmber
+// Boolean
+// nulll
+// unefined
+//symbol
+//Big int
+
+//refrence (Non primitive)
+
+// array 
+// object
+// functions
+//
+
+const score =100;
+const score1 ="hundred"
+
+//as we can see tht we dont need to define the type of datatype 
+//we didn't need to define in line 17 tht store int value or in line 18 
+//tht store string value it can adapt it according to the conditions
+
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id===anotherid)
+
+
+// JavaScript is a dynamically typed language,
+// which means that data types of variables are determined by
+// the value they hold at runtime and can change 
+// throughout the program as we assign different values to them.28
+
+const heroes =["shaktiman , nagraj , doggaa"];
+let myobj = {
+    name:"hitesh",
+    age:32,
+    //sex//:lucknow,
+
+}
+
+const myFunction=function(){
+    console.log("hello world");
+}
+
+
+
+
