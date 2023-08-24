3c7e8019fd1fd088a7bc892681e8ce8bfe706cd5
newwbiiee <142647441+newwbiiee@users.noreply.github.com>
2023-08-24 02:37:59 +0000

    container config done

---
+++ basics/c2.js
@@ -0,0 +1,43 @@
+const month =4;
+
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("febrary");
+        break;
+    case 3:
+        console.log("april");
+        break; 
+        
+    case 4:
+        console.log("May");
+        break;
+        
+    default:
+        console.log("default case method");
+        break;    
+
+
+        
+
+}
+
+const month1 = "sep";
+switch(month1){
+    case "jan":
+        console.log("January");
+        break;
+    case "feb" :
+        console.log("febraury");
+        break;
+        
+    case "mar":
+        console.log("March");
+        break;
+    case "sep":
+        console.log("found sep ");
+        break;    
+
+}
