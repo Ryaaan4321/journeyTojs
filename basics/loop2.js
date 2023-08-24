cc8e34044b8b06413fe1429c8e4c38aecf8a4e85
newwbiiee <142647441+newwbiiee@users.noreply.github.com>
2023-08-24 03:47:54 +0000

    container config done

---
+++ basics/loop2.js
@@ -0,0 +1,14 @@
+let myarr= ["batman", "spiderman" , "flash" , "loddu"];
+let arr=0;
+while(arr<myarr.length){
+    console.log(`value is ${myarr[arr]}`);
+    arr=arr+1;
+}
+
+let score =1;
+do{
+    console.log(`score is ${score}`);
+    score++;
+
+}
+while(score <=10);
